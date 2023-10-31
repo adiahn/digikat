b07134d9ca1eec9d216fd0b44b91b2ebccac4345
adiahn <adnanmukhtar2321@gmail.com>
2023-10-31 22:33:28 +0100

    Added the About us

---
+++ src/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='border-2 flex flex-col border-[red] w-full'>
+        <h1 className='text-5xl text-center'>ABOUT</h1>
+        <div className='flex flex-row flex-wrap justify-between p-10'>
+            <section className='border-2 border-[blue] w-5/12'>a</section>
+            <section className='border-2 border-[blue] w-5/12'>a</section>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
 import Overlay from "./Overlay/Overlay"
+import About from "./About/About"
 
 
 const App = () => {
   return(
-    <div>
+    <div className='box-border'>
       <Navbar />
       <Hero />
       <Overlay />
+      <About />
     </div>
   );
 }
