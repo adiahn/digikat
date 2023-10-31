6d64716dc9d236be5b9da64ad60749a9a4e6eb8a
codeknight1 <uthmanabdurrahman98@gmail.com>
2023-10-31 13:49:07 -0700

    latest2

--- src/Overlay/Overlay.jsx
+++ src/Overlay/Overlay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Overlay = () => {
     return(
         <div className="w-[1240px] h-[418px] bg-indigo-100 rounded-[10px] m-auto relative top-[-10rem] flex flex-row gap-4" >
-          <div className="w-full text-center p-4">
+          <div className="w-full text-center p-4q">
             <h1 className="text-sky-900 text-4xl font-bold font-['Inter']">VISION</h1>
             <p className="text-indigo-950 text-xl text-start font-medium font-['Poppins'] leading-[34px] tracking-wide">The project will help the state to become one of the largest digital consumer bases in the country, provide opportunities for local-level businesses to become global and provides and ecosystem that supports talented and innovative citizens growth and success.</p>
           </div>
