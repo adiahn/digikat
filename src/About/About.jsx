3a3715cf709ab079c2d3d3f726cdffefdeb7d907
adiahn <adnanmukhtar2321@gmail.com>
2023-11-01 23:25:59 +0100

    Removed Border and added the HE picture

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import He from "./he.jpeg"
 
 const About = () => {
   return (
-    <div className='border-2 flex flex-col border-[red] w-full'>
+    <div className='flex flex-col w-full'>
         <h1 className='text-5xl text-center'>ABOUT</h1>
         <div className='flex flex-row flex-wrap justify-between p-5'>
-            <section className='border-2 border-[blue] w-7/12 h-96'>
-                <p className='text-lg text-justify'><span className='text-2xl font-bold'>"DIGIKAT" </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nesciunt magni 
+            <section className='w-8/12 h-96'>
+                <p className='text-lg text-justify p-5'><span className='text-2xl font-bold'>"DIGIKAT" </span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nesciunt magni 
                     molestias tenetur incidunt ea quasi laudantium tempora aperiam dolor eveniet, 
                     reiciendis dolore perspiciatis facilis repellendus aut totam suscipit ducimus!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nesciunt magni 
@@ -23,7 +24,9 @@ const About = () => {
                     reiciendis dolore perspiciatis facilis repellendus aut totam suscipit ducimus!
                     </p>
             </section>
-            <section className='border-2 border-[blue] w-4/12'></section>
+            <section className='w-4/ flex justify-center'>
+                <img src={He} alt="" className='w-8/12 rounded-sm'/>
+            </section>
         </div>
     </div>
   )
