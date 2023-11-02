1416c502c1160d4407055d2322c2e8113ecd1215
codeknight1 <uthmanabdurrahman98@gmail.com>
2023-11-02 03:06:56 -0700

    new

--- src/Advisory/Advisory.jsx
+++ src/Advisory/Advisory.jsx
@@ -1,27 +1,48 @@
+// import React from 'react';
+// import TeamCard from "./Info";
+// import teamData from "./list.json";
+
+
+// function Implementation() { 
+//   return (
+//     <div className="container mx-auto mt-[5%] h-full">
+//       <div className="flex flex-col justiy-center items-center m-auto md:w-1/2">
+//             <div className="text-center  mb-12 ">
+//                <p className="text-[3em] font-bold text_gradient  text-primary">Advisory Board</p>
+//            </div>
+//             <p className="text-center">This is our stellar panel of advisors that bring diverse perspective and expertise and also lends their wisdom and guidance to the success of the project</p>
+//           </div>
+//       <div className="grid grid-cols-3 md:grid-cols-3 grid-flow-row gap-y-10 place-items-center mt-[4rem] md:w-[100%]">
+//         {teamData.map((member, index) => (
+//           <TeamCard key={index} data={member} />
+//         ))}
+//       </div>
+//     </div>
+   
+//   );
+// }
+
+// export default Implementation;
 import React from 'react';
-import TeamCard from "./Info";
-import teamData from "./list.json";
+import map2 from '../../public/map2.png'
+import Advisory2 from '../Advisory2/Advisory2';
 
 
-function Implementation() { 
+function Advisory() { 
   return (
-    <div className="container mx-auto mt-[5%] h-full">
-      <div className="flex flex-col justiy-center items-center m-auto md:w-1/2">
-            <div className="text-center  mb-12 ">
-               <p className="text-[3em] font-bold text_gradient  text-primary">Advisory Board</p>
+    <div className="w-full h-[250px] bg-indigo-200 bg-opacity-80 flex flex-row  mt-[5%] items-center relative">
+            <div className="flex flex-col w-1/2 p-2 ">
+               <p className="text-[1.5em] font-bold text_gradient text-center text-primary">Advisory Board</p>
+               <p className="text-center w-[60%] m-auto py-2">This is our stellar panel of advisors that bring diverse perspective and expertise and also lends their wisdom and guidance to the success of the project</p>
            </div>
-            <p className="text-center">This is our stellar panel of advisors that bring diverse perspective and expertise and also lends their wisdom and guidance to the success of the project</p>
-          </div>
-      <div className="grid grid-cols-3 md:grid-cols-3 grid-flow-row gap-y-10 place-items-center mt-[4rem] md:w-[100%]">
-        {teamData.map((member, index) => (
-          <TeamCard key={index} data={member} />
-        ))}
-      </div>
+           <img src={map2} alt="" className='absolute w-full h-full' />
+           <div className=" ">
+            <Advisory2 />
+           </div>
     </div>
    
   );
 }
 
-export default Implementation;
-
+export default Advisory;
 
